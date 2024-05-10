0e5b8e01cf1719781313f60ffec289c24fcb57ec
Isabela Ramos <isamrteixeira@gmail.com>
2024-05-10 15:58:21 -0300

    font family e nome do projeto

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Title from '../../components/Title'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/isabelaramoss.png" alt="" />
+    <Title>Isabela Ramos</Title>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Title from '../../components/Title'
+
+const Sobre = () => (
+  <section>
+    <Title>Sobre mim</Title>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
+      repellat enim culpa magnam voluptatum! Quos vitae voluptates, aut velit
+      nesciunt, quae mollitia iste, laudantium cum itaque odio. Error, eos
+      placeat.
+    </p>
+  </section>
+)
+
+export default Sobre
