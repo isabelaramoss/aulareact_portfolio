23391e32ca8d99575a5975aa17a2087163784ecb
Isabela Ramos <isamrteixeira@gmail.com>
2024-05-11 17:21:49 -0300

    componentes titulo e parágrafo

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,14 +1,15 @@
 import Title from '../../components/Title'
+import Paragrafo from '../../components/Paragrafo'
 
 const Sobre = () => (
   <section>
     <Title fontSize={16}>Sobre mim</Title>
-    <p>
+    <Paragrafo tipo="secundario">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
       repellat enim culpa magnam voluptatum! Quos vitae voluptates, aut velit
       nesciunt, quae mollitia iste, laudantium cum itaque odio. Error, eos
       placeat.
-    </p>
+    </Paragrafo>
   </section>
 )
 
