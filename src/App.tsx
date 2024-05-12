b18e929ad08d5fff5ccbc1f4264c521ef3e9718b
Isabela Ramos <isamrteixeira@gmail.com>
2024-05-12 17:57:13 -0300

    tema light

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { ThemeProvider } from 'styled-components'
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import temaLight from './themes/ligth'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaLight}>
       <EstiloGlobal />
       <Container>
         <Sidebar />
@@ -14,7 +16,7 @@ function App() {
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
