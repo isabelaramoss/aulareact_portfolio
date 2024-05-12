b7917a917d7dd17ede945b19480edf43e5b51196
Isabela Ramos <isamrteixeira@gmail.com>
2024-05-12 17:50:41 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Title from '../../components/Title'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/isabelaramoss.png" alt="" />
-    <Title fontSize={20}>Isabela Ramos</Title>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={20}>Isabela Ramos</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        @isabelaramoss
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
