c8e9fc45715472d9092381f16ea5e11f0889c8f4
Isabela Ramos <isamrteixeira@gmail.com>
2024-05-12 16:22:29 -0300

    seção sobre e responsividade

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Title from '../../components/Title'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -10,6 +11,10 @@ const Sobre = () => (
       nesciunt, quae mollitia iste, laudantium cum itaque odio. Error, eos
       placeat.
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=isabelaramoss&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=isabelaramoss&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
--- src/styles.ts
+++ src/styles.ts
@@ -9,7 +9,12 @@ const EstiloGlobal = createGlobalStyle`
 
   body{
     padding-top: 80px;
+
+    @media (max-width: 768px) {
+      padding-top: 16px;
+    }
   }
+
 `
 
 export default EstiloGlobal
@@ -22,8 +27,12 @@ export const Container = styled.div`
   grid-template-columns: 128px auto;
   column-gap: 56px;
 
+  @media (max-width: 768px) {
+    max-width: 80%;
+    display: block;
+  }
+
   img {
     max-width: 100%;
-    border-radius: 50%;
   }
 `
